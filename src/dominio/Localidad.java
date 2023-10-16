aaa15adc67582089af4e6c571c57b6907b7adf34
CARLOTA RUIZ CAMPESINO <carlota.ruizcampesino@usp.ceu.es>
2023-10-16 11:14:52 +0200

    commit1

---
+++ src/dominio/Localidad.java
@@ -0,0 +1,12 @@
+package dominio;
+
+        
+        public class Localidad{
+		private (String[] int nombre){
+	}
+	public Localidad (String[] nombre){
+		get Nombre.nombre;
+		get Nombre.nombre;
+ } 
+
+}
